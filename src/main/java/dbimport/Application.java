e26ecdce39b31419fce683d6c0f9107a253b08ab
Reomor <reomor@gmail.com>
2019-03-19 21:04:41 +0300

    logback support

--- src/main/java/dbimport/Application.java
+++ src/main/java/dbimport/Application.java
@@ -1,11 +1,17 @@
 package dbimport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.info("Logger works");
         SpringApplication.run(Application.class, args);
     }
 }
