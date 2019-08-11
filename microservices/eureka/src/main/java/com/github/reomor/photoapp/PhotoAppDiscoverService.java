a5ae603e048cc8015cb6c386e9e9cfaea0b78654
Reomor <reomor@gmail.com>
2019-08-11 19:33:21 +0300

    eureka fix naming

--- microservices/eureka/src/main/java/com/github/reomor/photoapp/PhotoAppDiscoverService.java
+++ microservices/eureka/src/main/java/com/github/reomor/photoapp/PhotoAppDiscoverService.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class PhotoAppApplication {
+public class PhotoAppDiscoverService {
     public static void main(String[] args) {
-        SpringApplication.run(PhotoAppApplication.class, args);
+        SpringApplication.run(PhotoAppDiscoverService.class, args);
     }
 }
--- microservices/eureka/src/test/java/com/github/reomor/photoapp/PhotoAppDiscoverServiceTests.java
+++ microservices/eureka/src/test/java/com/github/reomor/photoapp/PhotoAppDiscoverServiceTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PhotoAppApplicationTests {
+public class PhotoAppDiscoverServiceTests {
 
     @Test
     public void contextLoads() {
