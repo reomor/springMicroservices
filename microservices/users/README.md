c6ad19847941c35ed493ff2371fc95ccc17b4dec
Reomor <reomor@gmail.com>
2019-08-12 21:14:45 +0300

    users-ms customization for multiple microservice running, ribbon load-balancer demo

---
+++ microservices/users/README.md
@@ -0,0 +1,4 @@
+```
+mvn spring-boot:run -Dspring-boot.run.arguments=--spring.application.instance_id=meme
+mvn spring-boot:run -Dspring-boot.run.arguments=--spring.application.instance_id=meme,--server.port=9999
+```
--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/controllers/UserController.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.github.reomor.photoapp.api.users.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String getStatus() {
-        return "Working";
+        return "Working on: " + environment.getProperty("local.server.port");
     }
 }
