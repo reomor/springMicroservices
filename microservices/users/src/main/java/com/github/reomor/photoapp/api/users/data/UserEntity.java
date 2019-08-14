f52614e910a1160a208c22e7737c4411d475a883
Reomor <reomor@gmail.com>
2019-08-14 22:23:21 +0300

    users-ms user entity and repo

---
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/data/UserEntity.java
@@ -0,0 +1,77 @@
+package com.github.reomor.photoapp.api.users.data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "USERS")
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = -8438468099838395447L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String passwordEncrypted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordEncrypted() {
+        return passwordEncrypted;
+    }
+
+    public void setPasswordEncrypted(String passwordEncrypted) {
+        this.passwordEncrypted = passwordEncrypted;
+    }
+}
