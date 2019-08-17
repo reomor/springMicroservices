74a61f5a3eb1e6be365d493115c0e10705369e7e
Reomor <reomor@gmail.com>
2019-08-17 20:44:21 +0300

    users-ms add web security

--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/UsersApplication.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/UsersApplication.java
@@ -3,10 +3,17 @@ package com.github.reomor.photoapp.api.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class UsersApplication {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
     }
--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/service/UserServiceImpl.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.github.reomor.photoapp.api.users.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,20 +14,22 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+        userDto.setEncryptedPassword(passwordEncoder.encode(userDto.getPassword()));
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPassword("mock"); //FIXME: 2019-08-14
         final UserEntity savedUser = userRepository.save(userEntity);
         return modelMapper.map(savedUser, UserDto.class);
     }
