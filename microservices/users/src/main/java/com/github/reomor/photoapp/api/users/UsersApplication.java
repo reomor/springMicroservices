27cc57213feebbec939545b835b39f2048c25588
Reomor <reomor@gmail.com>
2020-02-02 22:18:42 +0300

    users-ms feign client hystrix support

--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/UsersApplication.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/UsersApplication.java
@@ -3,6 +3,7 @@ package com.github.reomor.photoapp.api.users;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/data/AlbumsServiceClient.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/data/AlbumsServiceClient.java
@@ -6,11 +6,20 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = EndpointsConst.ALBUMS_MS)
+@FeignClient(name = EndpointsConst.ALBUMS_MS, fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+@Component
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
