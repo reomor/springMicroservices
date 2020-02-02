ca659ed00297bd3219f4b952a1886dd85a558a41
Reomor <reomor@gmail.com>
2020-02-02 22:58:45 +0300

    users-ms feign client hystrix fallback factory

--- microservices/users/src/main/java/com/github/reomor/photoapp/api/users/data/AlbumsServiceClient.java
+++ microservices/users/src/main/java/com/github/reomor/photoapp/api/users/data/AlbumsServiceClient.java
@@ -2,24 +2,50 @@ package com.github.reomor.photoapp.api.users.data;
 
 import com.github.reomor.photoapp.api.users.config.EndpointsConst;
 import com.github.reomor.photoapp.api.users.ui.model.AlbumResponseModel;
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = EndpointsConst.ALBUMS_MS, fallback = AlbumsFallback.class)
+@FeignClient(name = EndpointsConst.ALBUMS_MS, fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
+
 @Component
 class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
 
     @Override
+    public AlbumsServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+    private final Logger logger = LoggerFactory.getLogger(AlbumsServiceClientFallback.class);
+    private final Throwable cause;
+
+    public AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
+
+    @Override
     public List<AlbumResponseModel> getAlbums(String id) {
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            logger.error("404 error while get albums with message{}", cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error while get albums with message{}", cause.getLocalizedMessage());
+        }
         return new ArrayList<>();
     }
 }
