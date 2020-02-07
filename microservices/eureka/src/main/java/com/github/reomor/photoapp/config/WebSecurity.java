18b952d23079939145334b813f21e675fbbafc7a
Reomor <reomor@gmail.com>
2020-02-07 20:42:22 +0300

    eureka gateway user/password httpbase protect

---
+++ microservices/eureka/src/main/java/com/github/reomor/photoapp/config/WebSecurity.java
@@ -0,0 +1,16 @@
+package com.github.reomor.photoapp.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
